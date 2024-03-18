1b21f52c57d616369a5b625aeef00f50169ed494
jonghyeon <jonghyeonpark.work@gmail.com>
2024-03-18 13:41:27 +0900

    leetcode: clone graph

---
+++ leetcode/133.clone-graph.py
@@ -0,0 +1,139 @@
+#
+# @lc app=leetcode id=133 lang=python
+#
+# [133] Clone Graph
+#
+# https://leetcode.com/problems/clone-graph/description/
+#
+# algorithms
+# Medium (56.36%)
+# Likes:    9258
+# Dislikes: 3721
+# Total Accepted:    1.2M
+# Total Submissions: 2.1M
+# Testcase Example:  '[[2,4],[1,3],[2,4],[1,3]]'
+#
+# Given a reference of a node in a connected undirected graph.
+#
+# Return a deep copy (clone) of the graph.
+#
+# Each node in the graph contains a value (int) and a list (List[Node]) of its
+# neighbors.
+#
+#
+# class Node {
+# ⁠   public int val;
+# ⁠   public List<Node> neighbors;
+# }
+#
+#
+#
+#
+# Test case format:
+#
+# For simplicity, each node's value is the same as the node's index
+# (1-indexed). For example, the first node with val == 1, the second node with
+# val == 2, and so on. The graph is represented in the test case using an
+# adjacency list.
+#
+# An adjacency list is a collection of unordered lists used to represent a
+# finite graph. Each list describes the set of neighbors of a node in the
+# graph.
+#
+# The given node will always be the first node with val = 1. You must return
+# the copy of the given node as a reference to the cloned graph.
+#
+#
+# Example 1:
+#
+#
+# Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+# Output: [[2,4],[1,3],[2,4],[1,3]]
+# Explanation: There are 4 nodes in the graph.
+# 1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+# 2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+# 3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+# 4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val =
+# 3).
+#
+#
+# Example 2:
+#
+#
+# Input: adjList = [[]]
+# Output: [[]]
+# Explanation: Note that the input contains one empty list. The graph consists
+# of only one node with val = 1 and it does not have any neighbors.
+#
+#
+# Example 3:
+#
+#
+# Input: adjList = []
+# Output: []
+# Explanation: This an empty graph, it does not have any nodes.
+#
+#
+#
+# Constraints:
+#
+#
+# The number of nodes in the graph is in the range [0, 100].
+# 1 <= Node.val <= 100
+# Node.val is unique for each node.
+# There are no repeated edges and no self-loops in the graph.
+# The Graph is connected and all nodes can be visited starting from the given
+# node.
+#
+#
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+
+class Node(object):
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+
+        map = {}
+
+        def dfs(cur):
+            # been here, done that
+            if cur in map:
+                return map[cur]  # get the new node(mapped to old one)
+
+            # create a copy of current node
+            copy = Node(cur.val)
+
+            # map the copy to the original node
+            map[cur] = copy
+
+            for nei in cur.neighbors:
+                # copy the neighbor, if it's already in the map, it will be returned
+                copied_nei = dfs(nei)
+                copy.neighbors.append(copied_nei)
+
+            # return the copied node
+            return copy
+
+        # start from the original node(if node is valid)
+        return dfs(node) if node else None
+
+
+# @lc code=end
