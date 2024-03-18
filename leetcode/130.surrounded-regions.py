0894525d06f721f9b16275aa775e712277edede5
jonghyeon <jonghyeonpark.work@gmail.com>
2024-03-18 15:37:04 +0900

    leetcode: surrounded regions

---
+++ leetcode/130.surrounded-regions.py
@@ -0,0 +1,93 @@
+#
+# @lc app=leetcode id=130 lang=python
+#
+# [130] Surrounded Regions
+#
+# https://leetcode.com/problems/surrounded-regions/description/
+#
+# algorithms
+# Medium (38.85%)
+# Likes:    8368
+# Dislikes: 1756
+# Total Accepted:    672K
+# Total Submissions: 1.7M
+# Testcase Example:  '[["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]'
+#
+# Given an m x n matrix board containing 'X' and 'O', capture all regions that
+# are 4-directionally surrounded by 'X'.
+#
+# A region is captured by flipping all 'O's into 'X's in that surrounded
+# region.
+#
+#
+# Example 1:
+#
+#
+# Input: board =
+# [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+# Output:
+# [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+# Explanation: Notice that an 'O' should not be flipped if:
+# - It is on the border, or
+# - It is adjacent to an 'O' that should not be flipped.
+# The bottom 'O' is on the border, so it is not flipped.
+# The other three 'O' form a surrounded region, so they are flipped.
+#
+#
+# Example 2:
+#
+#
+# Input: board = [["X"]]
+# Output: [["X"]]
+#
+#
+#
+# Constraints:
+#
+#
+# m == board.length
+# n == board[i].length
+# 1 <= m, n <= 200
+# board[i][j] is 'X' or 'O'.
+#
+#
+#
+
+
+# @lc code=start
+class Solution:
+    def solve(self, board: list[list[str]]) -> None:
+        """
+        reverse thinking: remove all the "o"s that cannot be wrapped with "x"s first.
+        """
+        ROWS, COLS = len(board), len(board[0])
+
+        def capture(r, c):
+            if r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] != "O":
+                return
+            board[r][c] = "T"
+            capture(r + 1, c)
+            capture(r - 1, c)
+            capture(r, c + 1)
+            capture(r, c - 1)
+
+        # 1. (DFS) Capture unsurrounded regions (O -> T)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O" and (r in [0, ROWS - 1] or c in [0, COLS - 1]):
+                    capture(r, c)
+
+        # 2. Capture surrounded regions (O -> X)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+
+        # 3. Uncapture unsurrounded regions (T -> O)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "T":
+                    board[r][c] = "O"
+
+
+# @lc code=end
