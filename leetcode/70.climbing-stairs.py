0ddd1b719a2442d7e8011afa3cf63b31ecf2afaf
jonghyeon <jonghyeonpark.work@gmail.com>
2024-03-20 15:01:08 +0900

    leetcode: 1D-DP

---
+++ leetcode/70.climbing-stairs.py
@@ -0,0 +1,68 @@
+#
+# @lc app=leetcode id=70 lang=python
+#
+# [70] Climbing Stairs
+#
+# https://leetcode.com/problems/climbing-stairs/description/
+#
+# algorithms
+# Easy (52.82%)
+# Likes:    21439
+# Dislikes: 781
+# Total Accepted:    3.1M
+# Total Submissions: 5.9M
+# Testcase Example:  '2'
+#
+# You are climbing a staircase. It takes n steps to reach the top.
+#
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can
+# you climb to the top?
+#
+#
+# Example 1:
+#
+#
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+#
+#
+# Example 2:
+#
+#
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= n <= 45
+#
+#
+#
+
+
+# @lc code=start
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        one, two = 1, 1
+
+        for _ in range(n - 1):
+            one, two = one + two, one
+
+        return one
+
+
+# @lc code=end
