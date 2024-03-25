2dea78053eee39a7fb7a930b0ea30f01929efeaa
jonghyeon <jonghyeonpark.work@gmail.com>
2024-03-25 10:05:36 +0900

    leetcode: backtracking

---
+++ leetcode/17.letter-combinations-of-a-phone-number.py
@@ -0,0 +1,89 @@
+#
+# @lc app=leetcode id=17 lang=python3
+#
+# [17] Letter Combinations of a Phone Number
+#
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+#
+# algorithms
+# Medium (59.80%)
+# Likes:    18098
+# Dislikes: 966
+# Total Accepted:    2M
+# Total Submissions: 3.3M
+# Testcase Example:  '"23"'
+#
+# Given a string containing digits from 2-9 inclusive, return all possible
+# letter combinations that the number could represent. Return the answer in any
+# order.
+#
+# A mapping of digits to letters (just like on the telephone buttons) is given
+# below. Note that 1 does not map to any letters.
+#
+#
+# Example 1:
+#
+#
+# Input: digits = "23"
+# Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+#
+#
+# Example 2:
+#
+#
+# Input: digits = ""
+# Output: []
+#
+#
+# Example 3:
+#
+#
+# Input: digits = "2"
+# Output: ["a","b","c"]
+#
+#
+#
+# Constraints:
+#
+#
+# 0 <= digits.length <= 4
+# digits[i] is a digit in the range ['2', '9'].
+#
+#
+#
+
+from typing import List
+
+
+# @lc code=start
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        # map required?
+        nums = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+        res = []
+
+        # brute-forcing with backtracking?
+        def bt(i, curS):
+            if len(curS) == len(digits):
+                res.append(curS)
+                return
+
+            for c in nums[digits[i]]:
+                bt(i + 1, curS + c)
+
+        if digits:
+            bt(0, "")
+
+        return res
+
+
+# @lc code=end
