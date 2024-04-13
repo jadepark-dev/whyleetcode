ac62d086520e5074a3daef5390d083b4fb1ddbd1
jonghyeon <jonghyeonpark.work@gmail.com>
2024-04-13 15:51:44 +0900

    leetcode: pattern variation

--- leetcode/3.longest-substring-without-repeating-characters.py
+++ leetcode/3.longest-substring-without-repeating-characters.py
@@ -78,5 +78,28 @@ class Solution:
 
         return res
 
+        # identical but with different form for the right pointer
+
+        """
+        charSet = set()
+
+        l, r = 0, 0
+        res = 0
+
+        while r < len(s):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+
+            charSet.add(s[r])
+
+            res = max(res, r - l + 1)
+
+            r += 1
+
+        return res
+
+        """
+
 
 # @lc code=end
