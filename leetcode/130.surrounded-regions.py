e992e02c427b765ca70c4ebfa121b42b25ff316e
jonghyeon <jonghyeonpark.work@gmail.com>
2024-04-14 16:57:04 +0900

    leetcode: DFS review

--- leetcode/130.surrounded-regions.py
+++ leetcode/130.surrounded-regions.py
@@ -62,32 +62,42 @@ class Solution:
         """
         ROWS, COLS = len(board), len(board[0])
 
-        def capture(r, c):
-            if r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] != "O":
+        # directional adjacents
+        dirs = [
+            [1, 0],
+            [-1, 0],
+            [0, 1],
+            [0, -1],
+        ]
+
+        def dfs(r, c):
+            # when to stop
+            if r < 0 or r == ROWS or c < 0 or c == COLS or board[r][c] != "O":
                 return
+
             board[r][c] = "T"
-            capture(r + 1, c)
-            capture(r - 1, c)
-            capture(r, c + 1)
-            capture(r, c - 1)
 
-        # 1. (DFS) Capture unsurrounded regions (O -> T)
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "O" and (r in [0, ROWS - 1] or c in [0, COLS - 1]):
-                    capture(r, c)
+            for dr, dc in dirs:
+                dfs(r + dr, c + dc)
 
-        # 2. Capture surrounded regions (O -> X)
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
+        for i in range(ROWS):
+            for j in range(COLS):
+                # run dfs to find edge-started cells
+                # find all edge-started "O" and convert them to temp value "T"
+                if (i == 0 or i == ROWS - 1 or j == 0 or j == COLS - 1) and board[i][
+                    j
+                ] == "O":
+                    dfs(i, j)
 
-        # 3. Uncapture unsurrounded regions (T -> O)
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "T":
-                    board[r][c] = "O"
+        for i in range(ROWS):
+            for j in range(COLS):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if board[i][j] == "T":
+                    board[i][j] = "O"
 
 
 # @lc code=end
